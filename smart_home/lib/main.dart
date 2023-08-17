a587d640a5a26718fd1ab82a5b9e38c1e835fcd9
orket <developerorket@gmail.com>
2023-08-17 18:04:00 +0300

    home ui updates

--- smart_home/lib/main.dart
+++ smart_home/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
@@ -25,6 +27,106 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height / 2,
+                  child: Image.asset(
+                    'assets/images/image 7.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(
+                        0xff221d1c), // Equivalent to rgba(33, 29, 29, 0.00)
+                  ),
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height / 2 - 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 198,
+                      width: MediaQuery.of(context).size.width / 2 - 20,
+                      decoration: BoxDecoration(
+                          color: const Color(0xff282424),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                    Container(
+                      height: 198,
+                      width: MediaQuery.of(context).size.width / 2 - 20,
+                      decoration: BoxDecoration(
+                          color: const Color(0xff282424),
+                          borderRadius: BorderRadius.circular(20)),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height / 2 - 260,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff282424),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Column(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Main light',
+                            style: GoogleFonts.manrope(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                              color: const Color(0xffF8F8F8),
+                              letterSpacing: 1.2,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Slider(
+                                  thumbColor: Colors.white,
+                                  activeColor: Colors.orange,
+                                  overlayColor: MaterialStateColor.resolveWith(
+                                    (states) => const Color(0xff4b4238),
+                                  ),
+                                  value: .3,
+                                  onChanged: (val) {},
+                                ),
+                              ),
+                              SvgPicture.asset('assets/svgs/Lamp.svg')
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
