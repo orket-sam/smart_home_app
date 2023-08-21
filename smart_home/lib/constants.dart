6cfea49458df9ca0dc2a42c58f2d16697360d380
orket <developerorket@gmail.com>
2023-08-21 12:40:30 +0300

    home screen ui updates

---
+++ smart_home/lib/constants.dart
@@ -0,0 +1,5 @@
+// App constants Colors, values etc.
+
+import 'package:flutter/material.dart';
+
+Color kGrey = const Color(0xfff8f8f8);
--- smart_home/lib/main.dart
+++ smart_home/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:smart_home/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffFFB267)),
         useMaterial3: true,
       ),
       home: const HomeScreen(),
@@ -57,22 +58,20 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               children: [
                 SizedBox(height: MediaQuery.of(context).size.height / 2 - 40),
-                Row(
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      height: 198,
-                      width: MediaQuery.of(context).size.width / 2 - 20,
-                      decoration: BoxDecoration(
-                          color: const Color(0xff282424),
-                          borderRadius: BorderRadius.circular(20)),
+                    CustomCard(
+                      percentage: 34,
+                      subTitle: 'Mode 2',
+                      title: 'Humidifier Air',
+                      svgUrl: 'assets/svgs/humidity.svg',
                     ),
-                    Container(
-                      height: 198,
-                      width: MediaQuery.of(context).size.width / 2 - 20,
-                      decoration: BoxDecoration(
-                          color: const Color(0xff282424),
-                          borderRadius: BorderRadius.circular(20)),
+                    CustomCard(
+                      percentage: 67,
+                      subTitle: 'On',
+                      title: 'Purifier Air',
+                      svgUrl: 'assets/svgs/clean.svg',
                     )
                   ],
                 ),
@@ -80,45 +79,25 @@ class HomeScreen extends StatelessWidget {
                   height: 15,
                 ),
                 Container(
+                  padding: const EdgeInsets.only(
+                    right: 20,
+                    top: 10,
+                  ),
                   height: MediaQuery.of(context).size.height / 2 - 260,
                   width: double.infinity,
                   decoration: BoxDecoration(
                       color: const Color(0xff282424),
                       borderRadius: BorderRadius.circular(20)),
-                  child: Column(
+                  child: const Column(
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Main light',
-                            style: GoogleFonts.manrope(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              fontStyle: FontStyle.normal,
-                              color: const Color(0xffF8F8F8),
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: Slider(
-                                  thumbColor: Colors.white,
-                                  activeColor: Colors.orange,
-                                  overlayColor: MaterialStateColor.resolveWith(
-                                    (states) => const Color(0xff4b4238),
-                                  ),
-                                  value: .3,
-                                  onChanged: (val) {},
-                                ),
-                              ),
-                              SvgPicture.asset('assets/svgs/Lamp.svg')
-                            ],
-                          ),
-                        ],
-                      )
+                      SliderWidget(
+                        title: 'Main light',
+                        svgUrl: 'assets/svgs/Lamp.svg',
+                      ),
+                      SliderWidget(
+                        title: 'Floor lamp',
+                        svgUrl: 'assets/svgs/Tablelamp.svg',
+                      ),
                     ],
                   ),
                 ),
@@ -127,6 +106,149 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final int percentage;
+  final String title;
+  final String subTitle;
+  final String svgUrl;
+
+  const CustomCard({
+    required this.percentage,
+    required this.subTitle,
+    required this.title,
+    required this.svgUrl,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle smallTextStyle = GoogleFonts.manrope(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.12,
+      color: kGrey.withOpacity(
+        0.6,
+      ),
+    );
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      height: 198,
+      width: MediaQuery.of(context).size.width / 2 - 20,
+      decoration: BoxDecoration(
+        color: const Color(0xff282424),
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                '$percentage %',
+                style: GoogleFonts.manrope(
+                  fontWeight: FontWeight.w500,
+                  color: kGrey,
+                  fontSize: 32,
+                ),
+              ),
+              const Spacer(),
+              SvgPicture.asset(
+                svgUrl,
+                width: 24,
+                height: 24,
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+            ),
+            child: Text(
+              title,
+              style: smallTextStyle,
+            ),
+          ),
+          const Divider(
+            color: Color(
+              0xff393535,
+            ),
+            thickness: 1,
+          ),
+          Row(
+            children: [
+              Text(
+                subTitle,
+                style: smallTextStyle,
+              ),
+              const Spacer(),
+              Switch(
+                thumbColor: const MaterialStatePropertyAll(Color(0xff000000)),
+                activeColor: const Color(0xffFFB267),
+                value: true,
+                onChanged: (val) {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SliderWidget extends StatelessWidget {
+  final String title;
+  final String svgUrl;
+  const SliderWidget({
+    required this.svgUrl,
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+          ),
+          child: Text(
+            title,
+            style: GoogleFonts.manrope(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.normal,
+              color: const Color(0xffF8F8F8),
+              letterSpacing: 1.2,
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Slider(
+                thumbColor: Colors.white,
+                activeColor: const Color(0xffFFB267),
+                overlayColor: MaterialStateColor.resolveWith(
+                  (states) => const Color(0xff4b4238),
+                ),
+                value: .3,
+                onChanged: (val) {},
+              ),
+            ),
+            SvgPicture.asset(svgUrl)
+          ],
+        ),
+      ],
     );
   }
 }
